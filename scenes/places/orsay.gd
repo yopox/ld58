09cb845bd05fd7442d69688234896061b3a73093
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2025-10-05 18:41:58 +0200

    make it blow up

--- scenes/places/orsay.gd
+++ scenes/places/orsay.gd
@@ -1,11 +1,50 @@
-extends Node2D
+extends Location
+
+enum ExplosionState {
+	IDLE,
+	ONGOING,
+	AFTER,
+}
+
+var explosion_force := 150
+var gravity := Vector2(0, 300)
+var explosion_state = ExplosionState.IDLE
+var velocities: Array[Vector2] = []
+
+@onready var parts := [
+	$EiffelTower/Part1,
+	$EiffelTower/Part2,
+	$EiffelTower/Part3,
+	$EiffelTower/Part4,
+]
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	await get_tree().create_timer(2).timeout
+	trigger_explosion()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func trigger_explosion() -> void:
+	cpu_particles_2d.emitting = true
+	await get_tree().create_timer(.2).timeout
+
+	for p in parts:
+		var dir = Vector2(randf() * 2 - 1, randf() * -2).normalized()
+		velocities.append(dir * explosion_force)
+	explosion_state = ExplosionState.ONGOING
+
 func _process(delta: float) -> void:
-	pass
+	match explosion_state:
+		ExplosionState.ONGOING:
+			var finished = false
+			for i in velocities.size():
+				velocities[i] += gravity * delta
+				parts[i].position += velocities[i] * delta
+				finished = finished && parts[i].position.y < 0
+			if finished:
+				explosion_state = ExplosionState.AFTER
+		ExplosionState.AFTER:
+			for i in parts.size():
+				parts[i].visible += false
+				explosion_state = ExplosionState.IDLE
