e704b1c98d2cfca3c6cfba0c93d027e47e191ac9
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2025-10-06 15:05:06 +0200

    fix louvre script

--- scenes/places/louvre.gd
+++ scenes/places/louvre.gd
@@ -2,7 +2,7 @@ extends Location
 
 const RAT = preload("uid://c3gdepdxmt38g")
 
-@onready var test: Actor = $Test
+@onready var music_player: Actor = $MusicPlayer
 @onready var rat_path: Path2D = $RatPath
 
 const SPAWN_INTERVAL := 0.5
@@ -15,29 +15,25 @@ var max_progress := 0.0
 var followers: Array[PathFollow2D] = []
 
 func _process(delta: float) -> void:
-	if max_progress > 1.0:
-		return
-
-	spawn_timer += delta
-	if spawn_timer >= SPAWN_INTERVAL:
-		spawn_timer = 0.0
-		spawn_rat()
-
-	for f in followers:
-		var rat = f.get_child(0)
-
-		var data = rat_path.curve.sample_baked_with_rotation(f.progress)
+	for rat in followers:
+		var data = rat_path.curve.sample_baked_with_rotation(rat.progress)
 		var angle = data.get_rotation()
 		rat.scale.x = sign(cos(angle)) if cos(angle) != 0 else 1
 		rat.rotation = PI / 2 * sign(cos(angle)) if sin(angle) >= 0 else 0.0
+	
+	if max_progress <= 1.0:
+		spawn_timer += delta
+		if spawn_timer >= SPAWN_INTERVAL:
+			spawn_timer = 0.0
+			spawn_rat()
 
 func spawn_rat() -> void:
 	var follow := PathFollow2D.new()
 	follow.loop = false
 	follow.rotates = false
-	follow.progress_ratio = 0.0
-	follow.z_index = RATS_COUNT- followers.size()
 	rat_path.add_child(follow)
+	follow.progress_ratio = 0.0
+	follow.z_index = RATS_COUNT - followers.size()
 
 	var rat := RAT.instantiate()
 	follow.add_child(rat)
@@ -64,6 +60,5 @@ func _on_test_interact() -> void:
 		Wow even more lines
 		wtfffff.
 		""",
-		test.global_position
+		music_player.global_position
 	)
-	Log.info("finished")
