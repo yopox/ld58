99fc6e7c652118d9b93fe37ae217dc4f70d76953
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2025-10-06 14:11:04 +0200

    add rat dance

--- scenes/places/louvre.gd
+++ scenes/places/louvre.gd
@@ -7,16 +7,15 @@ const RAT = preload("uid://c3gdepdxmt38g")
 
 const SPAWN_INTERVAL := 0.5
 const FULL_ANIMATION_LENGTH := 15.0
-const RATS_COUNT := 30
-const SCREEN_MID_X := 180.0 # adjust for your viewport width
+const RATS_COUNT := 45
 
 var spawn_timer := 0.0
 var spawned := 0
-var max_progress := 1.0
+var max_progress := 0.0
 var followers: Array[PathFollow2D] = []
 
 func _process(delta: float) -> void:
-	if max_progress <= 0.0:
+	if max_progress > 1.0:
 		return
 
 	spawn_timer += delta
@@ -37,6 +36,7 @@ func spawn_rat() -> void:
 	follow.loop = false
 	follow.rotates = false
 	follow.progress_ratio = 0.0
+	follow.z_index = RATS_COUNT- followers.size()
 	rat_path.add_child(follow)
 
 	var rat := RAT.instantiate()
@@ -44,7 +44,7 @@ func spawn_rat() -> void:
 	rat.play("Walk")
 
 	var target_progress = max_progress
-	max_progress -= 1.0 / RATS_COUNT
+	max_progress += 1.0 / RATS_COUNT
 	spawned += 1
 	followers.append(follow)
 
@@ -52,14 +52,8 @@ func spawn_rat() -> void:
 	tween.tween_property(follow, "progress_ratio", target_progress, max_progress * FULL_ANIMATION_LENGTH)
 	tween.set_trans(Tween.TRANS_LINEAR)
 	tween.tween_callback(func ():
-		rat.play("Idle")
+		rat.play("Dance")
 	)
-
-	# Mirror/rotate based on half of screen
-	if rat.global_position.x < SCREEN_MID_X:
-		rat.scale.x = 1
-	else:
-		rat.scale.x = -1
 
 func _on_test_interact() -> void:
 	await Util.show_dialog(
