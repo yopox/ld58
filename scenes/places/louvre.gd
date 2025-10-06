12abc97c060aa3729317bf28ceea55f525e160ea
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2025-10-06 16:11:36 +0200

    note play mechanism

--- scenes/places/louvre.gd
+++ scenes/places/louvre.gd
@@ -8,25 +8,49 @@ enum RatState {
 	DONE,
 }
 
-const FULL_ANIMATION_LENGTH := 15.0
+const NOTE = preload("uid://crnpelln8qmhe")
+const NOTE_ARRIVAL_LENGTH := 4.0
+const NOTE_INTERVAL := 1.0
+const NOTE_QUEUE := ["A", "C", "D", "E", "A", "B"]
 const RAT = preload("uid://c3gdepdxmt38g")
 const RATS_COUNT := 45
-const SPAWN_INTERVAL := 0.5
 
+var FULL_ANIMATION_LENGTH := NOTE_QUEUE.size() * NOTE_INTERVAL + NOTE_ARRIVAL_LENGTH
+var RAT_ANIMATION_LENGTH := FULL_ANIMATION_LENGTH / 2
+var SPAWN_INTERVAL := RAT_ANIMATION_LENGTH / RATS_COUNT
+# @onready var audio_players = {
+# 	"a": $Audio_A,
+# 	"b": $Audio_B,
+# 	"c": $Audio_C,
+# 	"d": $Audio_D,
+# 	"e": $Audio_E
+# }
+var active_notes = {
+	"A": [],
+	"B": [],
+	"C": [],
+	"D": [],
+	"E": [],
+}
 var explosion_force := 150
 var followers: Array[PathFollow2D] = []
 var gravity := Vector2(0, 300)
 var max_progress := 0.0
+var note_index := 0
+var note_timer := 0.0
 var rat_state = RatState.BEFORE
 var spawn_timer := 0.0
 var spawned := 0
 var velocities: Array[Vector2] = []
 
-@onready var path_a: Path2D = $Notes/NoteA/PathA
-@onready var path_b: Path2D = $Notes/NoteB/PathB
-@onready var path_c: Path2D = $Notes/NoteC/PathC
-@onready var path_d: Path2D = $Notes/NoteD/PathD
-@onready var path_e: Path2D = $Notes/NoteE/PathE
+# seconds between notes
+@onready var paths = {
+	"A": $Notes/NoteA/PathA,
+	"B": $Notes/NoteB/PathB,
+	"C": $Notes/NoteC/PathC,
+	"D": $Notes/NoteD/PathD,
+	"E": $Notes/NoteE/PathE,
+}
 @onready var music_player: Actor = $MusicPlayer
 @onready var rat_path: Path2D = $RatPath
 @onready var notes: Node2D = $Notes
@@ -35,18 +59,34 @@ var velocities: Array[Vector2] = []
 func _process(delta: float) -> void:
 	match rat_state:
 		RatState.STARTED:
+			process_notes(delta)
+
+			var lost = false
+			var win = note_index >= NOTE_QUEUE.size()
+			for key in active_notes.keys():
+				for note in active_notes[key]:
+					win = false
+					if note.progress_ratio >= 1.0:
+						lost = true
+			if lost:
+				explode()
+
 			for rat in followers:
 				var data = rat_path.curve.sample_baked_with_rotation(rat.progress)
 				var angle = data.get_rotation()
 				rat.scale.x = sign(cos(angle)) if cos(angle) != 0 else 1
 				rat.rotation = PI / 2 * sign(cos(angle)) if sin(angle) >= 0 else 0.0
+				var tween = rat.get_meta("tween")
+				if tween.is_running():
+					win = false
 
 			if max_progress <= 1.0:
 				spawn_timer += delta
 				if spawn_timer >= SPAWN_INTERVAL:
 					spawn_timer = 0.0
 					spawn_rat()
-			else:
+
+			if win:
 				rat_state = RatState.DONE
 		RatState.EXPLODING:
 			var finished = true
@@ -76,22 +116,77 @@ func cleanup_rats() -> void:
 	spawned = 0
 	max_progress = 1.0
 	spawn_timer = 0.0
+	Util.cutscene_playing = false
 
 
 func explode() -> void:
 	rat_state = RatState.BEFORE_EXPLODING
+	for key in active_notes.keys():
+		for note in active_notes[key]:
+			note.queue_free()
+		active_notes[key].clear()
 	for rat in followers:
 		rat.get_child(0).play("Idle")
 		rat.scale.x = 1
 		rat.rotation = 0.0
-		var tween = rat.get_meta("tween", null)
-		if tween and tween.is_running():
+		var tween = rat.get_meta("tween")
+		if tween.is_running():
 			tween.stop()
 		var dir = Vector2(randf() * 2 - 1, randf() * -2).normalized()
 		velocities.append(dir * explosion_force)
 
 	await Util.wait(1)
 	rat_state = RatState.EXPLODING
+
+
+func fire_note_from_queue(note_char: String) -> void:
+	var note_instance = NOTE.instantiate()
+	note_instance.play(note_char)
+
+	var follow = PathFollow2D.new()
+	follow.rotates = false
+	paths[note_char].add_child(follow)
+	follow.add_child(note_instance)
+	note_instance.position = Vector2.ZERO
+
+	var tween := create_tween()
+	tween.tween_property(follow, "progress_ratio", 1.0, NOTE_ARRIVAL_LENGTH)
+	tween.set_trans(Tween.TRANS_LINEAR)
+	follow.set_meta("tween", tween)
+
+	# add to active notes array
+	active_notes[note_char].append(follow)
+
+
+func play_note(path_key: String) -> void:
+	var related_notes = active_notes[path_key]
+	if related_notes.size() > 0:
+		var note = related_notes.pop_front()
+		var tween = note.get_meta("tween")
+		if tween.is_running():
+			tween.stop()
+		note.queue_free()
+	# audio_players[path_key].play()
+
+
+func process_notes(delta) -> void:
+	if Input.is_action_just_pressed("left"):
+		play_note("A")
+	elif Input.is_action_just_pressed("down"):
+		play_note("B")
+	elif Input.is_action_just_pressed("right"):
+		play_note("C")
+	elif Input.is_action_just_pressed("up"):
+		play_note("D")
+	elif Input.is_action_just_pressed("start") or Input.is_action_just_pressed("a"):
+		play_note("E")
+
+	note_timer += delta
+	if note_timer >= NOTE_INTERVAL and note_index < NOTE_QUEUE.size():
+		var note_char = NOTE_QUEUE[note_index]
+		fire_note_from_queue(note_char)
+		note_index += 1
+		note_timer = 0.0
 
 
 func spawn_rat() -> void:
@@ -112,7 +207,7 @@ func spawn_rat() -> void:
 	followers.append(follow)
 
 	var tween := create_tween()
-	tween.tween_property(follow, "progress_ratio", target_progress, max_progress * FULL_ANIMATION_LENGTH)
+	tween.tween_property(follow, "progress_ratio", target_progress, max_progress * RAT_ANIMATION_LENGTH)
 	tween.set_trans(Tween.TRANS_LINEAR)
 	tween.tween_callback(
 		func():
@@ -122,6 +217,7 @@ func spawn_rat() -> void:
 
 
 func _on_test_interact() -> void:
+	Util.cutscene_playing = true
 	await Util.show_dialog(
 		"Flute player",
 		"""
@@ -133,5 +229,3 @@ func _on_test_interact() -> void:
 	)
 	rat_state = RatState.STARTED
 	notes.visible = true
-	await Util.wait(5)
-	explode()
