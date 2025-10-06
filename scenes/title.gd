1020ceea2f105880a08ecacdc2d1161127ed9a05
yopox <dev@yopox.fr>
2025-10-06 22:50:43 +0200

    title: add credits and game version

--- scenes/title.gd
+++ scenes/title.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@onready var version: Label = $Version
+
+func _ready() -> void:
+	version.text = ProjectSettings.get_setting("application/config/version")
+
 
 func _process(_delta: float) -> void:
 	if Util.screen_transition: return
